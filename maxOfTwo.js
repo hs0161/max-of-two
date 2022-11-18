6b547a28e36d730663efa5105f38e4fe577b2459
Hannah Song <hannahsong89@gmail.com>
2022-11-17 22:43:24 -0600

    first commit

---
+++ maxOfTwo.js
@@ -0,0 +1,13 @@
+let x = 5
+let y = 5
+
+// Write your code below this line.
+
+if(y > x){
+    console.log(y)
+} else if(y === x){
+    console.log("The values are identical")
+}
+else {
+    console.log(x)
+}
